5f32f35ca065219a05ac04bf98db6722cc6cf44c
SkyXU <skyxu@tcl.com>
2018-07-17 18:00:18 +0800

    fixed cross origin

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ let logger = require('morgan');
 let router = require('./routes/index');
 let mysql = require("mysql");
 
-function connectDb(){
-    db = mysql.createConnection({
+function connectDb() {
+  db = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: "803923",
@@ -16,11 +16,11 @@ function connectDb(){
   db.connect(handlerDbError);
   db.on("error", handlerDbError);
 }
-function handlerDbError (err){
-  if (err){
-    if (err.code === "PROTOCOL_CONNECTION_LOST"){
-       connectDb();
-    } else{
+function handlerDbError(err) {
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      connectDb();
+    } else {
       console.error(err.stack || err);
     }
   }
@@ -30,52 +30,75 @@ connectDb();
 
 db.query(
   "CREATE TABLE IF NOT EXISTS `rank` ("
-  + "`sid` int(8) unsigned NOT NULL AUTO_INCREMENT, "        
+  + "`sid` int(8) unsigned NOT NULL AUTO_INCREMENT, "
   + "`fbid` bigint(20) unsigned DEFAULT 0000000000, "
   + "`fbicon` varchar(255) DEFAULT '', "
   + "`fbname` varchar(100) DEFAULT '', "
   + "`score` INT(10) unsigned DEFAULT 0, "
   + "PRIMARY KEY(`sid`))"
   + "AUTO_INCREMENT=10000000"
-  + ";", 
-  (err)=>{
-      if (err){
-          console.log(err.message);
-          throw err;
-      }
+  + ";",
+  (err) => {
+    if (err) {
+      console.log(err.message);
+      throw err;
     }
+  }
 );
 
 let app = express();
 
-    // view engine setup
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jade');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-    app.use(logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    // 设置/routes/index文件为总的路由控制文件
-    // 在index文件中再进行统一的路由分发
-    router(app, db);
+//设置跨域访问
+app.use(function (req, res, next) {
+  if (req.method === "OPTIONS") {
+    let headers = {};
+    headers["Access-Control-Allow-Origin"] = "*";
 
-    // catch 404 and forward to error handler
-    app.use(function(req, res, next) {
-      next(createError(404));
-    });
+    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
 
-    // error handler
-    app.use(function(err, req, res, next) {
-      // set locals, only providing error in development
-      res.locals.message = err.message;
-      res.locals.error = req.app.get('env') === 'development' ? err : {};
+    headers["Access-Control-Allow-Credentials"] = false;
 
-      // render the error page
-      res.status(err.status || 500);
-      res.render('error');
-    });
+    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+
+    res.writeHead(200, headers);
+
+    res.end();
+  } else {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  }
+});
+
+// 设置/routes/index文件为总的路由控制文件
+// 在index文件中再进行统一的路由分发
+router(app, db);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- routes/work.js
+++ routes/work.js
@@ -2,9 +2,10 @@ let express = require('express');
 let router = express.Router();
 
 function generateUserName() {
+  let nameLen = 8;
   let arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   let fbname = "";
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < nameLen; i++) {
     let pos = Math.round(Math.random() * (arr.length - 1));
     fbname += arr[pos];
   }
